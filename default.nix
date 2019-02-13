efab139405d2ce66209c96ab3eea4c9965cef3c0
Christian Theune <ct@flyingcircus.io>
2019-02-13 10:54:57 +0100

    ensure purity about the nixpkgs we expect

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
-with import <nixpkgs> {};
+let
+  pkgs_18_09_src = (import <nixpkgs> {}).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "3b44ccd99";
+    sha256 = "17704307xdkxkgharwnlxg46fzchrfz28niprz4z3cjd50shf6hh";
+  };
+  pkgs_18_09 = import pkgs_18_09_src {};
+in
+with pkgs_18_09;
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
